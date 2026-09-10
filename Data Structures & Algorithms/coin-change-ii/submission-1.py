21f2ae7ad6c21bfa1b8c0eb726a5aa45f5beb834
ningocnguyen <ngocni.080805@gmail.com>
2026-09-10 13:58:11 -0400

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        # dp[a] is the amount of ways to make amount a
+        dp = [0]*(amount+1)
+        dp[0] = 1
+        
+        # count combination, not permutation
+        for c in coins: # process in strict order
+            for a in range(c, amount+1):
+                dp[a] += dp[a-c]
+        
+        return dp[amount]
+        # time: O(a*n)
+        # space: O(a)
